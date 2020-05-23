df99a49b0e59aa447118da2cc977e9149c529c42
Huang Weijie <h_weijie@yahoo.com>
2020-05-23 10:10:25 +0800

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    const svelte: string = "Svelte";
+    const typescript: string = "Typescript";
+
+    function plus(first: string, second: string): string {
+        return `${first} plus ${second}!`;
+    }
+</script>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
+
+<main>
+    <h1>{plus(svelte, typescript)}</h1>
+</main>
